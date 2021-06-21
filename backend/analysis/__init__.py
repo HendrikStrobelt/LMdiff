a41cb10159d338d9962542b0433142967b3e5652
Benjamin Hoover <benjamin.hoover@ibm.com>
2021-06-21 15:11:51 -0500

    Initial backend docs

--- backend/analysis/__init__.py
+++ backend/analysis/__init__.py
@@ -2,4 +2,4 @@ from .text_dataset import TextDataset
 from .analysis_pipeline import AutoLMPipeline, collect_analysis_info, analyze_text
 from .helpers import LMAnalysisOutput, LMAnalysisOutputH5, model_name2path, model_path2name
 from .analysis_results_dataset import H5AnalysisResultDataset
-from .create_dataset import create_text_dataset
+from .create_dataset import create_text_dataset_from_hf_datasets
