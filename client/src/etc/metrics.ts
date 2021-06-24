b23242935ed5cf0244a1c84a57545e3f6c07b702
Strobelt Hendrik <hendrik@strobelt.com>
2021-06-23 22:25:20 -0400

    barcharts? barcharts !

--- client/src/etc/metrics.ts
+++ client/src/etc/metrics.ts
@@ -1,11 +1,11 @@
 export const availableMetrics = [
-  {k: "avg_rank_diff", d: "Average Rank Diff"},
-  {k: "max_rank_diff", d: "Maximum Rank Diff"},
-  {k: "avg_clamped_rank_diff", d: "Average Clamped 50 Rank Diff"},
-  {k: "max_clamped_rank_diff", d: "Maximum Clamped 50 Rank Diff"},
-  {k: "avg_prob_diff", d: "Average Probability Diff"},
-  {k: "max_prob_diff", d: "Maximum Probability Diff"},
+  {k: "avg_rank_diff", d: "Average Rank Diff", t: 'rank'},
+  {k: "max_rank_diff", d: "Maximum Rank Diff", t: 'rank'},
+  {k: "avg_clamped_rank_diff", d: "Average Clamped 50 Rank Diff", t: 'rank'},
+  {k: "max_clamped_rank_diff", d: "Maximum Clamped 50 Rank Diff", t: 'rank'},
+  {k: "avg_prob_diff", d: "Average Probability Diff", t: 'prob'},
+  {k: "max_prob_diff", d: "Maximum Probability Diff", t: 'prob'},
   // "kl",
-  {k: "avg_topk_diff", d: "Average TopK List Diff"},
-  {k: "max_topk_diff", d: "Maximum TopK List Diff"}
+  {k: "avg_topk_diff", d: "Average TopK List Diff", t: 'topk'},
+  {k: "max_topk_diff", d: "Maximum TopK List Diff", t: 'topk'}
 ]
